5a7bd6471f7299b49bf4ef092edbacb99411d7de
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-12-02 13:54:25 -0500

    [games] Added POST and GET routes for games played.

--- routes/games.js
+++ routes/games.js
@@ -7,26 +7,63 @@ const axios = require('axios');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
 router.get('/played', isLoggedIn, (req, res) => {
-    res.render('games/played')
+  db.user.findOne({
+    where: { id: req.user.id },
+    include: db.game
+
+    
+  }).then((user) => {
+    // console.log(user);
+    // db.game.findAll({
+    //   where: {userId: user.id}
+    // })
+    // .then((allGames => {
+    //   console.log(allGames);
+    //   res.render('games/played')
+    // }))
+
+    if (!user) throw Error()
+    db.game.findAll().then((allGames => {
+      let playedGames = allGames.filter((cat) => {
+        return user.games.map((c) => c.id).includes(cat.id)
+      })
+      console.log(playedGames);
+      res.render('games/played', {games: playedGames})
+
+    }))
+    
+  })
 })
 
 router.post('/played', isLoggedIn, (req, res) => {
-  db.game.findOrCreate({
-    where: {
-      rawg: req.body.gameRawgId
-    }
-  })
+  db.user.findOne({
+    where: { id: req.user.id }
+  }).then((user) => {
+    db.game.findOrCreate({
+      where: {
+        rawg: req.body.gameRawgId
+      },
+      defaults: {
+        title: req.body.gameTitle
+      }
+    }).then((gameReport) => {
+      const game = gameReport[0]
+      game.addUser(user).then(() => {
+        res.redirect('played')
 
-  res.redirect('games/played')
+      })
+    })
+  })
+  
 })
 
 
 router.get('/results', (req, res) => {
-    console.log(req.query);
+    // console.log(req.query);
     axios.get(`https://api.rawg.io/api/games?key=${process.env.API_KEY}&search=${req.query.game}&page=1&page_size=25`)
     .then(response => {
       let results = response.data.results
-      console.log(response.data);
+      // console.log(response.data);
       res.render('games/results', { results })
   
     })
